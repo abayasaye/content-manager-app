82dc855ffabeb1e9b39ac2c0db667308fd38eb53
abayasaye <abay400@gmail.com>
2023-05-02 18:03:19 +0300

    dev-commit

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,24 @@ import Newsletter from "./components/Newsletter";
 import ResourceHighlight from "./components/ResourceHighlight";
 import ResourceList from "./components/ResourceList";
 
-import {resources} from "../api/data"
+export default function Home({ resources }) {
 
-
-export default function Home() {
   return (
     <Layout>
-      <ResourceHighlight 
-      resources={resources}
-      />
+      <ResourceHighlight resources={resources.slice(0, 2)} />
       <Newsletter />
-      <ResourceList 
-      resources={resources}
-      />
-      <Footer/>
+      <ResourceList resources={resources.slice(2)} />
+      <Footer />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const resData = await fetch("http://localhost:3001/api/resources");
+  const data = await resData.json();
+  return {
+    props: {
+      resources: data,
+    },
+  };
 }
