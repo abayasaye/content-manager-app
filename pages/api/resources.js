7ba34680b6f54253dc6e6f184b253622ebcbffa6
abayasaye <abay400@gmail.com>
2023-05-03 18:02:19 +0300

    htttp-methods

--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,5 +1,24 @@
+import axios from "axios";
+
+export default async function (req, res,) {
+  if (req.method === "GET") {
+    const dataRes = await fetch("http://localhost:3001/api/resources");
+    const data = await dataRes.json();
+
+    return res.send(data);
+  }
+  if (req.method === "POST") {
+    const { title,description,link, priority, timeToFinish }= req.body;
+    if (!title || !description || !link || !priority || !timeToFinish) {
+        return res.status(422).json({ error: 'failed to load data' })
+    }
+    try {
+        const axiosRes = await axios.post("http://localhost:3001/api/resources", req.body)
+        return res.send(axiosRes.data);
+    } catch (error) {
+        return res.status(422).json({message:"data cannot be stored"})
+    }
 
 
-export default function (req , res){
-    res.send("in progress")
-} 
+  }
+}
--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,49 +1,76 @@
 import Layout from "../components/Layout";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
+const DEFAULT_DATA = {
+  title: "",
+  description: "",
+  link: "",
+  priority: "2",
+  timeToFinish: 60,
+};
 const ResourceCreate = () => {
+  const [form, setForm] = useState(DEFAULT_DATA);
+  const router = useRouter();
+
+  const submitHandler = () => {
+    axios.post("/api/resources", form)
+    .then(_=>router.push("/"))
+    .catch((err)=>{err?.response.data})
+  };
+
+  const cancelHandler = () => setForm(DEFAULT_DATA);
+
+  const handleChanges = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+//   cnost 
   return (
     <Layout>
-      <div className="container mt-6">
+      <div className="container m-6 p-6">
         <div className="columns">
           <div className="column is-8 is-offset-2">
             <div className="resource-form">
               <h1 className="title"> Add new resources</h1>
-              <form className="p-6">
-                <div class="field">
+              <form className="">
+                <div className="field">
                   <label className="label">title</label>
                   <div className="control">
                     <input
+                      name="title"
+                      onChange={handleChanges}
                       className=" input "
                       type="text"
+                      value={form.title}
                       placeholder="Learn Next JS and Sanity IO"
                     />
                   </div>
                 </div>
-                <div class="field">
-                  <label className="label">description</label>
-                  <div className="control">
-                    <input
-                      className=" input "
-                      type="text"
-                      placeholder="Learn these technologies because they are popular"
-                    />
-                  </div>
-                </div>
-                <div class="field">
+                <div className="field">
                   <label className="label">link</label>
                   <div className="control">
                     <input
+                      name="link"
+                      onChange={handleChanges}
+                      value={form.link}
                       className=" input "
                       type="text"
                       placeholder="https://example.com"
                     />
                   </div>
                 </div>
-                <div class="field">
-                  <label class="label">Priotity</label>
-                  <div class="control">
-                    <div class="select">
-                      <select>
+                <div className="field">
+                  <label className="label">Priotity</label>
+                  <div className="control">
+                    <div className="select">
+                      <select
+                        name="priority"
+                        onChange={handleChanges}
+                        value={form.priority}
+                      >
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
@@ -51,22 +78,50 @@ const ResourceCreate = () => {
                     </div>
                   </div>
                 </div>
-                <div class="field">
+                <div className="field">
                   <label className="label">timeToFinish</label>
                   <div className="control">
                     <input
+                      name="timeToFinish"
+                      onChange={handleChanges}
                       className=" input "
-                      type="text"
-                      placeholder="60 (time is in minutes)"
+                      type="number"
+                      value={form.timeToFinish}
+                      placeholder="60"
+                    />
+                  </div>
+                  <p className="help">Time is in minutes</p>
+                </div>
+                <div className="field">
+                  <label className="label">description</label>
+                  <div className="control">
+                    <textarea
+                      name="description"
+                      onChange={handleChanges}
+                      value={form.description}
+                      className="textarea"
+                      placeholder="Learn these technologies because they are popular"
                     />
                   </div>
                 </div>
                 <div className="field is-grouped">
                   <div className="control">
-                    <button className="button is-link">Submit</button>
+                    <button
+                      onClick={submitHandler}
+                      type="button"
+                      className="button is-link"
+                    >
+                      Submit
+                    </button>
                   </div>
                   <div className="control">
-                    <button className="button is-link is-light">Cancel</button>
+                    <button
+                      type="button"
+                      onClick={cancelHandler}
+                      className="button is-link is-light"
+                    >
+                      Cancel
+                    </button>
                   </div>
                 </div>
               </form>
