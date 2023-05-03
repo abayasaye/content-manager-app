6cde997a3fc03697dd833244b8b257aa53979a72
abayasaye <abay400@gmail.com>
2023-05-03 12:17:15 +0300

    adding-form

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,12 +1,14 @@
+import Link from "next/link";
+
 const Navbar = () => {
-    return (
-        <>
-                  <nav className="navbar">
+  return (
+    <>
+      <nav className="navbar">
         <div className="container">
           <div className="navbar-brand">
-            <a className="navbar-item" href="../">
+            <Link className="navbar-item" href="/">
               <h1>Content Manager</h1>
-            </a>
+            </Link>
             <span className="navbar-burger burger" data-target="navbarMenu">
               <span></span>
               <span></span>
@@ -27,12 +29,18 @@ const Navbar = () => {
                   </span>
                 </div>
               </div>
-              <a className="navbar-item is-active is-size-5 has-text-weight-semibold">
+              <Link
+                href="/"
+                className="navbar-item is-active is-size-5 has-text-weight-semibold"
+              >
                 Home
-              </a>
-              <a className="navbar-item is-size-5 has-text-weight-semibold">
-                Examples
-              </a>
+              </Link>
+              <Link
+                href="/resources/new"
+                className="navbar-item is-size-5 has-text-weight-semibold"
+              >
+                Add
+              </Link>
               <a className="navbar-item is-size-5 has-text-weight-semibold">
                 Features
               </a>
@@ -40,8 +48,8 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
-        </>
-    );
-}
+    </>
+  );
+};
 
 export default Navbar;
--- pages/resources/new.js
+++ pages/resources/new.js
@@ -1,13 +1,81 @@
 import Layout from "../components/Layout";
 
 const ResourceCreate = () => {
-    return (
-        <Layout>
-            <div>
-                resources create page 
+  return (
+    <Layout>
+      <div className="container mt-6">
+        <div className="columns">
+          <div className="column is-8 is-offset-2">
+            <div className="resource-form">
+              <h1 className="title"> Add new resources</h1>
+              <form className="p-6">
+                <div class="field">
+                  <label className="label">title</label>
+                  <div className="control">
+                    <input
+                      className=" input "
+                      type="text"
+                      placeholder="Learn Next JS and Sanity IO"
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <label className="label">description</label>
+                  <div className="control">
+                    <input
+                      className=" input "
+                      type="text"
+                      placeholder="Learn these technologies because they are popular"
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <label className="label">link</label>
+                  <div className="control">
+                    <input
+                      className=" input "
+                      type="text"
+                      placeholder="https://example.com"
+                    />
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Priotity</label>
+                  <div class="control">
+                    <div class="select">
+                      <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="field">
+                  <label className="label">timeToFinish</label>
+                  <div className="control">
+                    <input
+                      className=" input "
+                      type="text"
+                      placeholder="60 (time is in minutes)"
+                    />
+                  </div>
+                </div>
+                <div className="field is-grouped">
+                  <div className="control">
+                    <button className="button is-link">Submit</button>
+                  </div>
+                  <div className="control">
+                    <button className="button is-link is-light">Cancel</button>
+                  </div>
+                </div>
+              </form>
             </div>
-        </Layout>
-    );
-}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default ResourceCreate;
